7fa797a95bb0228e38cbe196df2ab85bf4d85830
Jason Evans <jason.evans>
2021-09-12 20:54:00 +0200

    Small visual changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,16 +38,16 @@ namespace touchsides
             
             this.wordStats.Clear();
             this.wordStats = words.GroupBy(word => word).Select(w => new WordDetail(w.Key, WordScoring.getWordScore(w.Key), w.Count())).OrderByDescending(word => word.Occurrences).ToList();
-            var temp = wordStats.Select(f => $"{f.Word} - Occurrences:{f.Occurrences}, Score: {f.Score}").ToArray();
-            FileContentsTextBox.Text = string.Join('\n', temp);
+            var WordDetails = wordStats.Select(f => $"{f.Word} - Occurrences:{f.Occurrences}, Score: {f.Score}").ToArray();
+            FileContentsTextBox.Text = string.Join('\n', WordDetails);
 
             var MaxOccurrences = this.wordStats.Max(f => f.Occurrences);
             var Max7CharOccurrences = this.wordStats.Where(wd => wd.Word.Length == 7).Max(f => f.Occurrences);
             var MaxScore = this.wordStats.Max(f => f.Score);
 
-            mostFrequentTB.Text = $"{ String.Join(", ", this.wordStats.Where(wordDetail => wordDetail.Occurrences == MaxOccurrences).Select(f => f.WordTrim())) } \nWith {MaxOccurrences} occurrence(s)";
+            mostFrequentTB.Text = $"{ String.Join(", ", this.wordStats.Where(wordDetail => wordDetail.Occurrences == MaxOccurrences).Select(f => f.Word.Trim())) } \nWith {MaxOccurrences} occurrence(s)";
 
-            mostFrequent7CharTB.Text = $"{ String.Join(", ", this.wordStats.Where(wd => wd.Word.Length == 7 && wd.Occurrences == Max7CharOccurrences).Select(wd => wd.WordTrim())) } \n\nWith {Max7CharOccurrences} occurrence(s)";
+            mostFrequent7CharTB.Text = $"{ String.Join(", ", this.wordStats.Where(wd => wd.Word.Length == 7 && wd.Occurrences == Max7CharOccurrences).Select(wd => wd.Word.Trim())) } \nWith {Max7CharOccurrences} occurrence(s)";
 
             highestScoreTB.Text = $"{ String.Join(", ", this.wordStats.Where(wd => wd.Score == MaxScore).Select(wd => wd.Word.Trim()))} \nWith a word score of {MaxScore}";
         }
